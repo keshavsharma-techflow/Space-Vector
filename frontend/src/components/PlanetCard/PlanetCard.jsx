5bd2044627d26df20dd09282ff1caf8e126d7735
Keshav Sharma <ks8295476@gmail.com>
2026-02-21 21:45:55 +0530

    Use dynamic API URL for background image

--- frontend/src/components/PlanetCard/PlanetCard.jsx
+++ frontend/src/components/PlanetCard/PlanetCard.jsx
@@ -1,11 +1,11 @@
 import styles from "./PlanetCard.module.css";
-
+ const apiUrl = import.meta.env.VITE_API_URL;
 
 
 export default function PlanetCard({obj,active,setActive,unique_id,navFun}){
  
     return(
-        <div  className={`${styles.cardContainer} ${active == unique_id?styles.cardEffect:styles.remCardEffect }`} style={{backgroundImage:`url(http://localhost:8080/uploads/${obj.img_url})`}} onClick={() => { setActive((prevId) => {if(prevId == unique_id){return -1} return unique_id}), navFun() }}>
+        <div  className={`${styles.cardContainer} ${active == unique_id?styles.cardEffect:styles.remCardEffect }`} style={{backgroundImage:`url(${apiUrl}uploads/${obj.img_url})`}} onClick={() => { setActive((prevId) => {if(prevId == unique_id){return -1} return unique_id}), navFun() }}>
             <p>{obj.heading}</p>
 
         </div>
